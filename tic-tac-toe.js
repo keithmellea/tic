94f4c36f9a99c186a496810157af8516eea33f28
jose <antonysoly2217@gmail.com>
2021-02-04 18:46:28 -0500

    second

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,13 +1,29 @@
 window.addEventListener('DOMContentLoaded', event => {
 
-   
-    console.log(xDiv);
+
+    // console.log(xDiv);
     const click = document.getElementById("tic-tac-toe-board");
-
+    console.log(click)
+    const squares = document.querySelectorAll(".square");
+    console.log(squares)
     click.addEventListener("click", event => {
-    const xDiv = document.createElement('img');
-    xDiv.src = "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg"
 
+        const xDiv = document.createElement('img');
+        xDiv.src = "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg"
+        const oDiv = document.createElement('img');
+        oDiv.src = "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg"
+        let turnCount = 0;
+        for (let i = 0; i < squares.length; i++){
+            let square = squares[i];
+
+            if(event.target === square && turnCount%2 !== 0){
+                square.appendChild(xDiv)
+            } else if(event.target === square && turnCount%2 === 0){
+                square.appendChild(oDiv)
+            }
+            turnCount++;
+
+        }
 
     })
 
