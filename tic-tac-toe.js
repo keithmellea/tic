7d6dd4214256c44baf77128dfdef9affae09afdb
Keith Mellea <keithmellea@gmail.com>
2021-02-04 18:25:56 -0500

    first commit

---
+++ tic-tac-toe.js
@@ -0,0 +1,15 @@
+window.addEventListener('DOMContentLoaded', event => {
+
+   
+    console.log(xDiv);
+    const click = document.getElementById("tic-tac-toe-board");
+
+    click.addEventListener("click", event => {
+    const xDiv = document.createElement('img');
+    xDiv.src = "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg"
+
+
+    })
+
+
+})
